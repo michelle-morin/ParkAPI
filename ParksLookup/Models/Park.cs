ca26614d4d9d74fd65ccb91478a26b4ef0b064a3
Michelle Morin <morin2m@gmail.com>
2020-04-03 10:00:36 -0700

    add validations to model

--- ParksLookup/Models/Park.cs
+++ ParksLookup/Models/Park.cs
@@ -1,14 +1,22 @@
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ParksLookup.Models
 {
   public class Park
   {
     public int ParkId { get; set; }
+    [Required]
+    [StringLength(100)]
     public string Name { get; set; }
+    [Required]
+    [StringLength(100)]
     public string Agency { get; set; }
+    [Required]
+    [StringLength(100)]
     public string State { get; set; }
+    [Required]
     public string Description { get; set; }
+    [Required]
     public bool Campsites { get; set; }
   }
 }
