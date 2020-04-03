3610b53e6dbf833afa374e3f60d516340e31ec93
Michelle Morin <morin2m@gmail.com>
2020-04-03 08:51:30 -0700

    add park model

---
+++ ParksLookup/Models/Park.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace ParksLookup.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string Agency { get; set; }
+    public string State { get; set; }
+    public string Description { get; set; }
+    public int Campsites { get; set; }
+  }
+}
