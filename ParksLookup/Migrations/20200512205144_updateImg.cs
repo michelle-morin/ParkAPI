d169591d324f982947912e0662160edb00989507
Michelle Morin <morin2m@gmail.com>
2020-05-12 14:30:11 -0700

    update migrations for new seed data
    Co-authored-by: Drake Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>
    Co-authored-by: Brandan Sayarath <brandan@brandan.tech>

---
+++ ParksLookup/Migrations/20200512205144_updateImg.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksLookup.Migrations
+{
+    public partial class updateImg : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4,
+                column: "ImgUrl",
+                value: "https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1200,w_1600,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/0617-forest-park-stone-house_w82t7g.jpg");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4,
+                column: "ImgUrl",
+                value: "https://kval.com/resources/media/a89b1f7d-ec51-4562-9357-0acecd06a98d-large16x9_24.CraterLakeNationalPark.jpg?1531428301707");
+        }
+    }
+}
